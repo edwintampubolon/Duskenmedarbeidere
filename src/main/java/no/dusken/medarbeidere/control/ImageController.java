ba10d009be07a09fb3d64e394fd19088dcfc200a
Marvin B. Lillehaug <marvin.lillehaug@gmail.com>
2012-01-02 12:57:09 +0100

    images

--- src/main/java/no/dusken/medarbeidere/control/ImageController.java
+++ src/main/java/no/dusken/medarbeidere/control/ImageController.java
@@ -1,5 +1,7 @@
 package no.dusken.medarbeidere.control;
 
+import no.dusken.common.util.ImageReader;
+import no.dusken.common.util.impl.ImageReaderImpl;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,8 +9,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
 import java.io.*;
 
 @Controller
@@ -18,8 +23,13 @@ public class ImageController {
     @Value("#{ dataDir }/image")
     private File imagefolder;
 
+    @Value("#{ dataDir }/image/cache")
+    private File imageCachefolder;
+
+    ImageReader imageReader = new ImageReaderImpl();
+
     @RequestMapping("/image/{path}")
-    public void handleImage(@PathVariable String path, HttpServletResponse response) throws IOException {
+    public void handleImage(@PathVariable String path, @RequestParam(required = false, defaultValue = "-1") Integer height, @RequestParam(required = false, defaultValue = "-1") Integer width, HttpServletResponse response) throws IOException {
         File image = new File(imagefolder, path.replace("_", "/"));
         if (!image.exists() || !image.canRead()) {
             log.warn("Error reading: {}", image);
@@ -30,6 +40,12 @@ public class ImageController {
             }
             return;
         }
+
+        File cache = getCacheFile(height, width);
+        if (cache == null) return;
+
+        image = getOrCreateCachedFile(path, height, width, cache);
+
         response.setContentType("image/jpeg");
         response.setHeader("Cache-Control", "public, max-age=92505600");
         response.setContentLength((int) image.length());
@@ -37,5 +53,26 @@ public class ImageController {
         OutputStream outputStream = response.getOutputStream();
         InputStream inputStream = new FileInputStream(image);
         IOUtils.copy(inputStream, outputStream);
+    }
+
+    private File getOrCreateCachedFile(String path, Integer height, Integer width, File cache) throws IOException {
+        File image;
+        image = new File(cache, path);
+        if(!image.exists()){
+            BufferedImage bufferedImage = imageReader.readImage(image, height, width);
+            ImageIO.write(bufferedImage, "jpeg", image);
+        }
+        return image;
+    }
+
+    private File getCacheFile(Integer height, Integer width) {
+        File cache = new File(imageCachefolder, height + "x" + width);
+        if(!cache.exists()){
+            if (!cache.mkdirs()){
+                log.error("Could not create folder {}", cache.getAbsolutePath() );
+                cache = null;
+            }
+        }
+        return cache;
     }
 }
--- src/main/webapp/WEB-INF/jsp/medarbeider/include/personElement.jsp
+++ src/main/webapp/WEB-INF/jsp/medarbeider/include/personElement.jsp
@@ -1,5 +1,5 @@
 <li><article class="searchPerson">
-    <a href="/medarbeidere/${person.username}"><img src="/image/medarbeider_${person.username}.jpg" alt="${person.firstname} ${person.surname}"></a>
+    <a href="/medarbeidere/${person.username}"><img src="/image/medarbeider_${person.username}.jpg?height=150" alt="${person.firstname} ${person.surname}"></a>
     <a href="/medarbeidere/${person.username}">${person.fullName}</a>
     <label for="${person.username}mail">Epost:</label>
     <a href="mailto:${person.username}@underdusken.no" id="${person.username}mail">${person.username}@underdusken.no</a>
